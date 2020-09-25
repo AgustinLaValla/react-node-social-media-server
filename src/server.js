bb9f9de592915b2844b615b1076ed4d5192d6929
Agustín La Valla <agulavalla@gmail.com>
2020-09-25 01:25:01 -0300

    json parse bug fixed

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const io = socketIo(server);
 app.set('port', process.env.PORT || 4000);
 
 //Middlewares
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({origin:true, credentials:true}));
 
